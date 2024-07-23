d065c730405a4a21803e264b59e2b70248fc4b01
Kamil Matyasik <matyasik.k@wp.pl>
2024-07-23 21:28:11 +0200

    added our sigbn in component as well as firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sing-in.component";
 
 const Shop = () => {
   return <h1>Shop component</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
       {/* <Route path="/hats" element={...} */}
     </Routes>
